3c92b6ccf930bb79cc5491c89d52d663ad2987c2
maneshadesilva99-code <maneshadesilva99@gmail.com>
2026-03-13 10:14:37 +0530

    db fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,11 @@ import { Pool } from "pg";
 import * as schema from "./schema";
 import { ENV } from "../config/env";
 
-if (!ENV.DATABASE_URI) {
+if (!ENV.DATABASE_URL) {
     throw new Error("DATABASE_URI is not set in environment variables")
 }
 
-const pool = new Pool({ connectionString: ENV.DATABASE_URI });
+const pool = new Pool({ connectionString: ENV.DATABASE_URL });
 
 pool.on("connect", () => {
     console.log("Database connected successfully");
